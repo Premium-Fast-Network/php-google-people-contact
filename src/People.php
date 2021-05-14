1ccb483aca573516c617f3ceffcf7a3b00267b3e
JuniYadi <juniyadi94@gmail.com>
2021-05-15 05:09:14 +0700

    Update People

--- src/People.php
+++ src/People.php
@@ -10,6 +10,9 @@ class People
     const PeopleV1 =
         'https://people.googleapis.com/v1';
 
+    /**
+     * Default Person Fields
+     */
     const PersonFields = ['addresses', 'ageRanges', 'biographies', 'birthdays', 'calendarUrls', 'clientData', 'coverPhotos', 'emailAddresses', 'events', 'externalIds', 'genders', 'imClients', 'interests', 'locales', 'locations', 'memberships', 'metadata', 'miscKeywords', 'names', 'nicknames', 'occupations', 'organizations', 'phoneNumbers', 'photos', 'relations', 'sipAddresses', 'skills', 'urls', 'userDefined',];
 
     private $client;
@@ -20,8 +23,6 @@ class People
         $this->client = $client;
         // set default root url
         $this->client->setRootURL(self::PeopleV1);
-        // fetch access_token from refresh token if not exist
-        $this->client->validateToken();
         // set scopes default
         if(empty($this->client->getScopes())) {
             $this->client->setScopes([
@@ -30,20 +31,58 @@ class People
                 Scopes::CONTACTS_READONLY,
             ]);
         }
+        // fetch access_token from refresh token if not exist
+        $this->client->validateToken();
     }
 
-    public function getContact($value)
+    public function getContact($resourceName)
     {
-        return $this->client->request('GET', $value.'?personFields=' . implode(',', self::PersonFields));
+        return $this->client->request(
+            'GET',
+            $resourceName.'?personFields=' .
+            implode(',', self::PersonFields)
+        );
     }
     
-    public function createContact($value)
+    public function createContact($options)
     {
+        return $this->client->request(
+            'POST',
+            'people:createContact?personFields=' . implode(',', self::PersonFields), 
+            $options
+        );
+    }
 
+    public function deleteContact($resourceName)
+    {
+        return $this->client->request(
+            'GET',
+            $resourceName.':deleteContact'
+        );
     }
 
     public function listContact($options)
     {
-        return $this->client->request('GET', 'people/me/connections?personFields=' . implode(',', self::PersonFields) .'&'. http_build_query($options));
+        return $this->client->request(
+            'GET',
+            'people/me/connections?personFields=' .
+                implode(',', self::PersonFields) .
+                '&'. http_build_query($options)
+            );
+    }
+
+    public function batchCreateContact()
+    {
+
+    }
+
+    public function batchUpdateContact()
+    {
+
+    }
+
+    public function batchDeleteContact()
+    {
+
     }
 }
